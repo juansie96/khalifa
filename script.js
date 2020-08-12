5197fbb6f4ebb212e566b7e61584b3d04321a240
Pulga <juansie.96@gmail.com>
2020-08-12 15:28:11 -0300

    Khalifa V 1.0

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+const $submitBtn = document.querySelector('#submit-btn');
+
+
+
+window.onload = () => {
+    $submitBtn.disabled = false;
+}
+
 function handleSubmit(){
+    $submitBtn.disabled = true;
     event.preventDefault();
     const data = new FormData(event.target);
     fetch('https://script.google.com/macros/s/AKfycbwm-HJW9XXnR293dYAeqVsijofW0JuGIekHoZQAaQ/exec', {
